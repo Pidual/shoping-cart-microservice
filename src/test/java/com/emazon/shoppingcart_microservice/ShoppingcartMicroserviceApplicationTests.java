dd1dede8a496bc41ac88c72e48a5f6de0ae0dcd8
Carlos <73614255+Pidual@users.noreply.github.com>
2024-09-15 13:03:57 -0500

    spring init files

---
+++ src/test/java/com/emazon/shoppingcart_microservice/ShoppingcartMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emazon.shoppingcart_microservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingcartMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
